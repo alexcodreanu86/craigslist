786e14cdaeb7a2beeaf9c9da2cf72021606a612b
Apprentice <apprentice@dbc-chicago-029-34.local>
2014-02-18 16:42:01 -0600

    seeded some posts and some categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Category.create(name: "Medication")
 Category.create(name: "pisici")
-
+Post.create(name: "black cat", description: "a nice black cat",email: "joe@shcmo.com",price: 20,category_id: 2)
+Post.create(name: "black cat", description: "a nice black cat",email: "joe@shcmo.com",price: 20,category_id: 1)
 
