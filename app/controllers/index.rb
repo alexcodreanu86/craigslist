625e1b60d9c963d123f9250fd64f8b5b10c21104
Apprentice <apprentice@dbc-chicago-029-34.local>
2014-02-18 17:09:12 -0600

    Create category page
    in index.rb:
      added 'get categories/:id'
    in views:
      added 'categories.erb'

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,13 @@
 get '/' do
   # Look in app/views/index.erb
-  @categories = Category.all.map{|category| category.name}
+  @categories = Category.all #.map{|category| category.name}
   erb :index
 end
+
+get '/categories/:id' do
+  @category = Category.find(params[:id].to_i)
+  @posts = @category.posts
+  erb :category
+end
+
+
